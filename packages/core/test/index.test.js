cc8d57021e8ebc2c2e90a019e1b65ca1ec98f238
Axelen123 <axelakesson05@hotmail.com>
2020-11-25 22:08:36 +0100

    core: use dirty-chai

--- packages/core/test/index.test.js
+++ packages/core/test/index.test.js
@@ -1,10 +1,12 @@
 const chai = require('chai');
 const sinon = require('sinon');
+const dirtyChai = require('dirty-chai');
 const sinonChai = require('sinon-chai');
 
 const core = require('../');
 
 chai.should();
+chai.use(dirtyChai);
 chai.use(sinonChai);
 
 describe('@guardianjs/core', () => {
@@ -12,19 +14,24 @@ describe('@guardianjs/core', () => {
         const falseSpy = sinon.spy(() => false);
         const trueSpy = sinon.spy(() => true);
 
-        // Register routes
+        // Register routes.
         core.register("/false", falseSpy);
         core.register("/true", trueSpy);
 
         // "fake" has not been registered
         core.check("/fake").should.equal(true);
 
+        // register() shouldn't call and check() should only call on match.
+        falseSpy.should.not.have.been.called();
+        trueSpy.should.not.have.been.called();
+
+        // Check access.
         core.check("/false").should.equal(false);
         core.check("/true").should.equal(true);
 
-        falseSpy.should.have.callCount(1);
-        trueSpy.should.have.callCount(1);
-
+        // Make sure that the guards have only been called once, and in the right order.
+        falseSpy.should.have.been.calledOnce();
+        trueSpy.should.have.been.calledOnce();
         falseSpy.should.have.been.calledBefore(trueSpy);
     });
 });
